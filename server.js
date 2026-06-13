a9cbe81585b73007920ba1d9023da61bb7efbdcc
code5tomono11-wq <code.5.tomo.no.11@gmail.com>
2026-06-13 19:12:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
 
 app.get("/", (req,res)=>{
-    res.send("ぬいチャン成功！");
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("TEST SERVER");
+server.listen(process.env.PORT || 3000,()=>{
+    console.log("server start");
 });
