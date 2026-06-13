58f6f28a367c26dd08fb7a50c7ca70640c42aa49
code5tomono11-wq <code.5.tomo.no.11@gmail.com>
2026-06-13 18:54:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,11 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(__dirname));
 
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname + "/index.html");
+    res.send("ぬいチャン成功！");
 });
 
-let rooms = {};
-
-io.on("connection",(socket)=>{
-
-    socket.on("createRoom",()=>{
-
-        let room =
-        Math.floor(1000 + Math.random()*9000)
-        .toString();
-
-        rooms[room]=[];
-
-        socket.join(room);
-
-        rooms[room].push(socket.id);
-
-        socket.emit("roomCreated",room);
-    });
-
-    socket.on("joinRoom",(room)=>{
-
-        if(!rooms[room]){
-            socket.emit(
-                "errorMessage",
-                "部屋がありません"
-            );
-            return;
-        }
-
-        socket.join(room);
-
-        rooms[room].push(socket.id);
-
-        io.to(room).emit("playerJoined");
-    });
-
-});
-
-server.listen(process.env.PORT || 3000,()=>{
-    console.log("server start");
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("TEST SERVER");
 });
