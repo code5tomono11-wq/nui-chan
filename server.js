1845db1ced1220bf2064fd84855de781c3ca5cb7
code5tomono11-wq <code.5.tomo.no.11@gmail.com>
2026-06-17 21:02:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.use(express.static("public"));
+app.use(express.static("."));
 
 http.listen(process.env.PORT || 3000);
 
@@ -12,25 +12,18 @@ io.on("connection",(socket)=>{
     console.log("接続");
 
     socket.on("createRoom",(room)=>{
-
         socket.join(room);
-
     });
 
     socket.on("joinRoom",(room)=>{
-
         socket.join(room);
-
         io.to(room).emit("battleStart");
-
     });
 
     socket.on("move",(data)=>{
-
         socket.to(data.room).emit("enemyMove",{
             damage:data.damage
         });
-
     });
 
 });
