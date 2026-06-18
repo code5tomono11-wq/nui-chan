42c3fa28df5a0bf9a1680ea36504e8d92006cbcb
code5tomono11-wq <code.5.tomo.no.11@gmail.com>
2026-06-19 06:22:08 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,3 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-
-app.use(express.static("."));
-
-http.listen(process.env.PORT || 3000);
-
-io.on("connection",(socket)=>{
-
-    console.log("接続");
-
-    socket.on("createRoom",(room)=>{
-        socket.join(room);
-    });
-
-    socket.on("joinRoom",(room)=>{
-        socket.join(room);
-        io.to(room).emit("battleStart");
-    });
-
-    socket.on("move",(data)=>{
-
-        socket.to(data.room).emit("enemyMove",{
-            damage:data.damage
-        });
-
-    });
-    
-    socket.on("teamData",(data)=>{
-
-    socket.to(data.room).emit("enemyTeam",data);
-
-});
-    
 socket.on("selectTeam",(data)=>{
 
     console.log("チーム受信",data);
@@ -44,13 +8,3 @@ socket.on("selectTeam",(data)=>{
     );
 
 });
-    
-    socket.on("win",(room)=>{
-
-        socket.to(room).emit("lose");
-
-    });
-
-});
-
-
